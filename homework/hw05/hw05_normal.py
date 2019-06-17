97afe32749bab9e50061b4ce9d80d642487f5c9a
Vladislav Lubomski <vlad.lubomski@gmail.com>
2019-06-17 20:15:12 +0400

    hw05 normal_2 - Решение с помощью регулярки

--- homework/hw05/hw05_normal.py
+++ homework/hw05/hw05_normal.py
@@ -1,3 +1,5 @@
+import re
+
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
@@ -19,6 +21,31 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+# решение с помощью re
+print(re.findall(r'[a-z]+', line))
+
+# решение без re
+lowercase = []  # список, куда будем добавлять строчные буквы
+switch = 1  # переключатель - индикатор партии строчных (0) или заглавных (1) букв
+i = 0  # индекс для создания вложенных списков с партиями строчных букв
+for id_ch, ch in enumerate(line):
+    if ch.islower() and switch == 1:  # если буква строчная и перед ней была заглавная -
+        lowercase.append([])  # создаём новый вложенный список
+        lowercase[i].append(ch)  # добавляем туда эту букву
+        switch = 0  # переключатель в положение "строчные"
+    elif ch.islower() and switch == 0:  # если строчная и перед ней были строчные
+        lowercase[i].append(ch)  # просто добавляем в последний вложенный список
+    elif ch.isupper() and id_ch < len(line) - 1 and line[id_ch + 1].islower():
+        # если же буква заглавная, и не последняя в списке, а так же следующая буква - строчная
+        i += 1  # индекс списка увеличиваем на один, чтобы следующая строчная попала в новый вложенный список
+        switch = 1  # свитч устанавливаем в положение "заглавные"
+    else:  # если же буква заглавная, без других условий
+        switch = 1  # свитч устанавливаем в положение "заглавные"
+
+new_line = [''.join(x) for x in lowercase]
+
+print(new_line)
 
 
 # Задание-2:
@@ -44,6 +71,10 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+# решение с помощью re
+print(re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2))
+
 
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
