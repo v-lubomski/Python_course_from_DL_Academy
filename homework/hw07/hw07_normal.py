b8d57f33d9db7698e4c6862905b4b5b7baedc118
Vladislav Lubomski <vlad.lubomski@gmail.com>
2019-06-24 01:59:50 +0400

    Started hw07_normal

--- homework/hw07/hw07_normal.py
+++ homework/hw07/hw07_normal.py
@@ -15,3 +15,69 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+
+class People:
+    def __init__(self, name, father_name, surname):
+        self.name = name
+        self.surname = surname
+        self.patronymic = father_name
+
+    @property
+    def full_name(self):
+        return self.name + ' ' + self.patronymic + ' ' + self.surname
+
+    @property
+    def short_name(self):
+        return '{} {}.{}.'.format(self.surname.title(), self.name[0].upper(), self.patronymic[0].upper())
+
+
+class Student(People):
+    def __init__(self, name, father_name, surname, mom, dad, school_class):
+        People.__init__(self, name, father_name, surname)
+        self.mom = mom
+        self.dad = dad
+        self.school_class = school_class
+
+
+class Teacher(People):
+    def __init__(self, name, father_name, surname, subject):
+        People.__init__(self, name, father_name, surname)
+        self.subject = subject
+
+
+class ClassRooms:
+    def __init__(self, class_room, teachers):
+        self.class_room = class_room
+        self.teachers_dict = {t.subject: t for t in teachers}
+
+
+teachers = [Teacher('Иван', 'Иванович', 'Иванов', 'Математика'),
+            Teacher('Петр', 'Петрович', 'Петров', 'Литература'),
+            Teacher('Сидор', 'Сидорович', 'Сидоров', 'Физика'),
+            Teacher('Дмитрий', 'Дмитриевич', 'Дмитриев', 'История'),
+            Teacher('Никита', 'Никитиевич', 'Никишин', 'Биология')]
+classes = [ClassRooms('11 А', [teachers[0], teachers[1], teachers[2]]),
+           ClassRooms('11 Б', [teachers[1], teachers[3], teachers[4]]),
+           ClassRooms('10 А', [teachers[3], teachers[1], teachers[0]])]
+parents = [People('Семен', 'Семенович', 'Семенов'),
+           People('Светлана', 'Савельевна', 'Семенова'),
+           People('Роман', 'Романович', 'Романов'),
+           People('Римма', 'Романовна', 'Романова'),
+           People('Сергей', 'Сергеевич', 'Сергеев'),
+           People('Юлия', 'Викторвна', 'Сергеева')]
+students = [Student('Игорь', 'Cеменович', 'Семенов', parents[0], parents[1], classes[0]),
+            Student('Ольга', 'Романова', 'Романова', parents[2], parents[3], classes[1]),
+            Student('Александр', 'Сергеевич', 'Сергеев', parents[4], parents[5], classes[2])]
+print('Список классов в школе: ')
+for f in classes:
+    print(f.class_room)
+
+for f in classes:
+    print('Учителя, преподающие в {} классе:'.format(f.class_room))
+    for teacher in classes[0].teachers_dict.values():
+        print(teacher.full_name)
+for f in classes:
+    print("Ученики в классе {}:".format(f.class_room))
+    for st in students:
+        print(st.short_name)
